50ede14cbcd70e790e499b250d4d658e029e335d
Sourabh Bhide <61515923+sourabh-bhide@users.noreply.github.com>
2020-12-28 18:06:06 +0100

    Update tissue2cells.py

--- tissue2cells.py
+++ tissue2cells.py
@@ -162,29 +162,6 @@ def measure_props(start,stop,master_data):
     measured_data.to_csv('measured_data.csv', sep=';', decimal=',')
     print('DONE')
 
-def get_concat_h_multi_resize(im_list, resample=Image.BICUBIC):
-    min_height = min(im.height for im in im_list)
-    im_list_resize = [im.resize((int(im.width * min_height / im.height), min_height),resample=resample)
-                      for im in im_list]
-    total_width = sum(im.width for im in im_list_resize)
-    dst = Image.new('RGB', (total_width, min_height))
-    pos_x = 0
-    for im in im_list_resize:
-        dst.paste(im, (pos_x, 0))
-        pos_x += im.width
-    return dst
-
-def get_concat_v_multi_resize(im_list, resample=Image.BICUBIC):
-    min_width = min(im.width for im in im_list)
-    im_list_resize = [im.resize((min_width, int(im.height * min_width / im.width)),resample=resample)
-                      for im in im_list]
-    total_height = sum(im.height for im in im_list_resize)
-    dst = Image.new('RGB', (min_width, total_height))
-    pos_y = 0
-    for im in im_list_resize:
-        dst.paste(im, (0, pos_y))
-        pos_y += im.height
-    return dst
 
 def get_concat_h_blank(im1, im2, color=(0, 0, 0)):
     dst = Image.new('RGB', (im1.width + im2.width, max(im1.height, im2.height)), color)
